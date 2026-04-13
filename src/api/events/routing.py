0f98b9c0d0fa4d697b2e80cd682a9271508a7b68
Rahul Jain <rahuljain0890@icloud.com>
2026-04-14 00:08:26 +0200

    connection to postgres is not successful

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,6 +1,9 @@
 import os
-from fastapi import APIRouter, routing
-from .schema import EventCreateSchema, EventListSchema, EventSchema, EventUpdateSchema
+from pydoc import importfile
+from fastapi import APIRouter, Depends, routing
+from .models import EventCreateSchema, EventListSchema, EventModel, EventUpdateSchema
+from api.db.session import get_session
+from sqlmodel import Session
 
 from fastapi import FastAPI
 
@@ -21,7 +24,7 @@ def read_events() -> EventListSchema:
 
 #GEt /api/events/
 @router.get("/{event_id}")
-def get_event(event_id:int) -> EventSchema: 
+def get_event(event_id:int) -> EventModel: 
     return {"id":event_id}
 
 
@@ -34,17 +37,22 @@ def get_event(event_id:int) -> EventSchema:
 #     return {"id":123}
 
 #Update the data
-@router.post("/")
-def create_event(event_id: int, payload:EventCreateSchema) -> EventSchema: 
-    print(payload.page)
+@router.post("/", response_model= EventModel)
+def create_event( 
+                    payload:EventCreateSchema, 
+                    session: Session = Depends(get_session)): 
     data = payload.model_dump()   #payload -> dict -> pydantic
-    return {"id":123, **data}
+    obj = EventModel.model_validate(data)
+    session.add(obj)
+    session.commit()
+    session.refresh(obj)
+    return obj
 
 #Update this data
 # PUT /api/events/12
 
 @router.put("/{event_id}")
-def update_event(event_id:int, payload: EventUpdateSchema) -> EventSchema: 
+def update_event(event_id:int, payload: EventUpdateSchema) -> EventModel: 
     print(payload.description)
     data = payload.model_dump()
     return {"id":event_id,  **data}
--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+from api.db.session import init_db
 
 
 from api.events import router as events_router
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # before app start up
+    init_db()
+    yield
+    #clean up
 
 app = FastAPI()
 app.include_router(events_router, prefix="/api/events")
